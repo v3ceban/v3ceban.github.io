800d0597d14fe68b31ee7876cf56b41b5a6679b1
Vladimir Ceban <v3ceban@gmail.com>
2023-04-11 22:16:39 -0700

    Updated web design section and index

--- js/webdesign.js
+++ js/webdesign.js
@@ -1,50 +1,18 @@
 function webDesign() {
-    const parentElement = document.querySelectorAll(`.left`)[0],
-        span = document.querySelectorAll(`.left span`)[0],
-        prev = document.querySelectorAll(`.fa-caret-left`)[0],
-        next = document.querySelectorAll(`.fa-caret-right`)[0],
-        elements = document.querySelectorAll(`#WebDesign .left div`),
-        textelements = document.querySelectorAll(`#WebDesign .right div`);
+    const elements = document.querySelectorAll(`#WebDesign .container .scroll-wrapper`);
 
-    var counter = 0;
-
-    parentElement.addEventListener('click', () => {
-        span.classList.add('fade-out');
-    });
-
-    parentElement.addEventListener('touchstart', () => {
-        span.classList.add('fade-out');
-    });
-
-    parentElement.addEventListener('scroll', () => {
-        span.classList.add('fade-out');
-    });
-
-    prev.addEventListener('click', () => {
-        elements[counter].classList.add(`inactive`);
-        textelements[counter].classList.add(`inactive`);
-        counter--;
-        if (counter < 0) {
-            counter = elements.length - 1;
+    function AEL(listener) {
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            element.addEventListener(listener, () => {
+                element.querySelector(`.hint-text`).classList.add('fade-out');
+            });
         }
-        elements[counter].classList.remove(`inactive`);
-        textelements[counter].classList.remove(`inactive`);
-        span.classList.remove('fade-out');
-        parentElement.scrollTop = 0;
-    });
+    }
 
-    next.addEventListener('click', () => {
-        elements[counter].classList.add(`inactive`);
-        textelements[counter].classList.add(`inactive`);
-        counter++;
-        if (counter == elements.length) {
-            counter = 0;
-        }
-        elements[counter].classList.remove(`inactive`);
-        textelements[counter].classList.remove(`inactive`);
-        span.classList.remove('fade-out');
-        parentElement.scrollTop = 0;
-    });
+    AEL('scroll');
+    AEL('touchstart');
+    AEL('click');
 }
 
 webDesign();
